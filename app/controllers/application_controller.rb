2aeee392eae592cee6accb53eee840c85be63315
ThomasDebek <tom.debek@gmail.com>
2025-06-13 22:39:11 +0200

    layout: set new views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+
+
+  before_action :set_filters_data
+
+  private
+
+  def set_filters_data
+    @brands = Brand.all
+    @categories = Category.all
+  end
 end
