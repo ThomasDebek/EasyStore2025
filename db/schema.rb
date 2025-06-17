5826b58df005201369ff8c3cb8332e3d317d7fb8
ThomasDebek <tom.debek@gmail.com>
2025-06-17 23:22:02 +0200

    delete: model product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_13_103048) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_17_211919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -66,18 +66,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_13_103048) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.decimal "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "brand_id", null: false
-    t.bigint "category_id", null: false
-    t.index ["brand_id"], name: "index_products_on_brand_id"
-    t.index ["category_id"], name: "index_products_on_category_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,6 +81,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_13_103048) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "products", "brands"
-  add_foreign_key "products", "categories"
 end
