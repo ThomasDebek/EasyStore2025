988c944ed883d2980bc3276b17a4b8cd38c45927
ThomasDebek <tom.debek@gmail.com>
2025-06-17 23:15:30 +0200

    delete: product

--- app/helpers/products_helper.rb
+++
@@ -1,9 +0,0 @@
-module ProductsHelper
-  def brands
-    @brands ||= Brand.all
-  end
-
-  def categories
-    @categories ||= Category.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'products#index'
-  resources :products
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
