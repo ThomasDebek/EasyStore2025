d1ed3f26971956aee1fe80410f05eed19e7c361f
ThomasDebek <tom.debek@gmail.com>
2025-06-27 11:04:15 +0200

    routes: set routes devise for logged google

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :categories
     resources :brands
   end
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :products, only: [:index, :show]
 
   root 'products#index', only: [:show, :index]
