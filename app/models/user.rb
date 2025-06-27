5e27c572c1291d7bae95b7ae7edfa4f54960dd92
ThomasDebek <tom.debek@gmail.com>
2025-06-27 11:05:06 +0200

    model_user: set method do logged

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,21 @@ class User < ApplicationRecord
   def admin?
     admin == true
   end
+
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user
+  end
+
+
+
 end
