dddc67a9089e1f0a0925864dcfb5b98466150a19
ThomasDebek <tom.debek@gmail.com>
2025-07-04 12:02:10 +0200

    model: rails g model Cart user:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_25_125218) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_04_094020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_catalog.plpgsql"
@@ -67,6 +67,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_25_125218) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -82,4 +89,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_25_125218) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "carts", "users"
 end
